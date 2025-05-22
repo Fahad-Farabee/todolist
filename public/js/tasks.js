9698606d37e19336281c3a5a496129f0c47a800c
Fahad-Farabee <smfahadfarabee@gmail.com>
2025-05-22 21:10:34 +0600

    have finished all the features of the task

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -3,20 +3,93 @@ document.addEventListener('DOMContentLoaded',function(){
         try{
             const response = await fetch('/api/tasks');
             const tasks = await response.json();
+            const taskBody = document.getElementById('task_body');
+            taskBody.innerHTML = '';
 
-            const taskList = document.getElementById('task-list');
-            taskList.innerHTML = ' ';
-            
-            if(tasks.length === 0){
-                taskList.innerHTML = '<li>No tasks are available.</li>';
+            if (tasks.length === 0) {
+                const tr = document.createElement('tr');
+                tr.innerHTML = `<td colspan="5">No tasks are available.</td>`;
+                taskBody.appendChild(tr);
                 return;
             }
-
+            //taking each task.
             tasks.forEach(task=>{
-                const li = document.createElement('li');
-                li.textContent = `${task.task_details} -> Due date: ${task.due_date_time} -> Status: ${task.task_status ? 'done':'pending'}`;
+                const tr = document.createElement('tr');
+                const tdDetails = document.createElement('td');
+                tdDetails.textContent = task.task_details;
+                const tdDueTime = document.createElement('td');
+                tdDueTime.textContent = task.due_date_time;
+                const tdStatus = document.createElement('td');
+                if(task.task_status){
+                    tdStatus.textContent = 'complete';
+                }else{
+                    tdStatus.textContent = 'pending';
+                }
+                
+                //for updating the task status.
+                const tdActions = document.createElement('td');
+                const btn = document.createElement('button');
+                btn.textContent = 'click me';
+                //making the  function for updating the status.
+                btn.addEventListener('click',async()=>{//adding the event listener for clicking the button.
+                    //doing the fetch.
+                    try{
+                        const updateResponse = await fetch(`/api/task-update/${task.id}`,{
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Accept': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                task_status: !task.task_status 
+                            })
+                        });
 
-                taskList.appendChild(li);
+                        if(updateResponse.ok){
+                            fetchTasks();//this will refresh the task list.
+                        }else{
+                            updateResponse.msg;
+                        }
+                    }catch(error){
+                        console.error('Error updating the task', error);
+                    }
+
+                });
+
+                //for deleting a task.
+                const tdDelete = document.createElement('td');
+                const dltBtn = document.createElement('button');
+                dltBtn.textContent = 'click me to delete';
+                dltBtn.addEventListener('click',async()=>{
+                    //doing the delete
+                    try{
+                        const dltResponse = await fetch(`/api/task-delete/${task.id}`,{
+                            method: 'DELETE',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Accept': 'application/json'
+                            },
+                        });
+                        if(dltResponse.ok){
+                            fetchTasks()
+                        }else{
+                            dltResponse.msg;
+                        }
+                    }catch(error){
+                        console.error('Error deleting the task', error);
+                    }
+                });
+
+
+                tdActions.appendChild(btn);
+                tdDelete.appendChild(dltBtn);
+                tr.appendChild(tdDetails);
+                tr.appendChild(tdDueTime);
+                tr.appendChild(tdStatus);
+                tr.appendChild(tdActions);
+                tr.appendChild(tdDelete);
+                taskBody.appendChild(tr);
+
             });
         }catch(err){
             console.error('Error Loading Task: ',err);
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -20,9 +20,41 @@
     <div id="massage" style="margin-top: 10px; font-weight: bold;"></div>
 
     <h3>My Tasks</h3>
-    <ul id="task-list">
+    <p></p>
+   {{--  <ul id="task-list">
         <li>Loading Tasks..</li>
-    </ul>
+    </ul> --}}
+    <table border="1">
+       <thead>
+         <tr>
+            <th>
+                Task
+            </th>
+            <th>
+                Due time
+            </th>
+            <th>
+                Status
+            </th>
+            <th>
+                Task button
+            </th>
+            <th>
+                Task Deletion
+            </th>
+        </tr>
+       </thead>
+
+        <tbody id="task_body">
+        <tr>
+            <td id="task_details">loading...</td>
+            <td id="task_due_time">loading...</td>
+            <td id="task_status">loading...</td>
+            <td><Button id="task_button">click</Button></td>
+            <td><Button id="task_button">click</Button></td>
+        </tr>
+        </tbody>
+    </table>
 
     <script src="{{asset('js/add_task.js')}}"></script>
     <script src="{{ asset('js/tasks.js') }}"></script>
